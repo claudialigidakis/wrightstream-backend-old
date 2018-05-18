6f9c58acc8efd7cf373e435134e6521f607c0dcd
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-05-18 15:28:41 -0700

    created the migrations for staff, roles, and shops. created seeds for routes. created all staff and shop routes except update

---
+++ src/models/shops.js
@@ -0,0 +1,134 @@
+const knex = require('../../db/knex');
+const bcrypt = require('bcrypt')
+
+function getShopByName(shopName) {
+  return (knex('shops')
+  .where({shop_name: shopName})
+  .first())
+}
+
+function getOneShop(shopsId) {
+  return (knex('shops')
+  .where({id: shopsId})
+  .first())
+}
+
+function createShop(body) {
+  console.log(body, "made it to createShop models")
+  let shopName = body.shop_name
+  let logo_url = body.logoURL
+  let email = body.email
+  var newShopId = ''
+
+  return getShopByName(shopName).then(data => {
+    console.log("made it back from getShopByName")
+    if (data) throw {status : 400, message: 'Shop exists'}
+    return (
+      knex('shops')
+      .insert({
+        shop_name: shopName,
+        logo_url: logo_url
+      })
+      .returning('*'))
+  }).then(data => {
+    let newShopId = data[0].id
+    return createStaff(body, newShopId)
+  }).then(function([{ password, ...data }]) {
+    return data
+  })
+}
+
+function updateShop() {
+
+}
+
+function removeShop(shopsId) {
+  return (
+  knex('staff')
+  .where({shops_id: shopsId})
+  .del()
+)
+.then(data =>{
+  return (
+    knex('shops')
+    .where({id: shopsId})
+    .del()
+  )
+})
+}
+
+//user routes
+
+function getStaffByEmail(email) {
+  console.log(email, "made it to getStaffByEmail")
+  let staffEmail = email
+  return (knex('staff').where({email: staffEmail}).first())
+}
+
+function createStaff(body, newShopId) {
+  console.log("made it to create staff", body, newShopId)
+  let password = body.password
+  let first_name = body.fname
+  let last_name = body.lname
+  let staffEmail = body.email
+  let photo_url = body.photo
+  let shopId = newShopId
+  let role = body.role || 1
+  return getStaffByEmail(staffEmail)
+  .then(data => {
+    if (data) throw { status : 400, message: 'Staff member already exists'}
+    return bcrypt.hash(password, 10)
+  }).then(newPassword => {
+    return (
+      knex('staff')
+    .insert({
+      shops_id: shopId,
+      role_id: role,
+      first_name: first_name,
+      last_name: last_name,
+      email: staffEmail,
+      password: newPassword,
+      photo: photo_url
+    }).returning('*'))
+  })
+}
+
+function getOneStaff(staffId, shopId) {
+  console.log("made it to get one staff models")
+  return (
+    knex('staff')
+  .where({
+    id: staffId,
+    shops_id: shopId
+  })
+  .first())
+}
+
+function getAllStaff(shopId) {
+  return (
+    knex('staff')
+  .where({shops_id: shopId})
+  .first())
+}
+
+function updateStaff() {
+
+}
+
+function removeStaff(staffId) {
+  return (knex('staff')
+  .where({id: staffId})
+  .del())
+}
+
+module.exports = {
+  getOneShop,
+  createShop,
+  removeShop,
+  updateShop,
+  createStaff,
+  getAllStaff,
+  getOneStaff,
+  updateStaff,
+  removeStaff
+}
---
+++ src/routes/shops.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const shopController = require('../controllers/shops')
+
+//shop routes
+router.get('/:shopId', shopController.getOneShop)
+router.post('/', shopController.createShop)
+router.put('/:shopId', shopController.updateShop)
+router.delete('/:shopId', shopController.removeShop)
+//staff routes
+router.get('/:shopId/staff/:staffId', shopController.getOneStaff)
+router.get('/:shopId/staff', shopController.getAllStaff)
+router.post('/:shopId/staff', shopController.createStaff)
+router.put('/:shopId/staff/:staffId', shopController.updateStaff)
+router.delete('/:shopId/staff/:staffId', shopController.removeStaff)
+
+module.exports = router
