0cf57228855d4ebfe0f0f342eb60128b7c083531
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-05-23 14:22:10 -0700

    reverted changes for shop routes

--- db/migrations/20180518083624_shops.js
+++ db/migrations/20180518083624_shops.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('shops', (table) => {
     table.increments();
     table.string('shop_name').notNullable();
-    table.string('settings')defaultsTo('');
-    table.text('logo_url')defaultsTo('');
+    table.string('settings').defaultsTo('');
+    table.text('logo_url').defaultsTo('');
   });
 };
 
