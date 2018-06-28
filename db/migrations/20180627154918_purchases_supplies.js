a8261935ae93ba28f0a010abcce5290135fe798b
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-06-28 10:51:18 -0700

    added more seed data and am beginning to create the wrightstream predictor

--- db/migrations/20180627154918_purchases_supplies.js
+++ db/migrations/20180627154918_purchases_supplies.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('purchases_supplies', (table) => {
     table.integer('purchase_id').nullable().references('purchases.id');
     table.integer('supplies_id').notNullable().references('supplies.id');
-    table.integer('supplies_qty').notNullable()
+    table.decimal('supplies_qty').notNullable()
     table.string('supplies_measurement').notNullable()
     table.boolean('completed').notNullable().defaultsTo(false);
     table.timestamps('true', 'true');
--- db/seeds/22-purchases_supplies.js
+++ db/seeds/22-purchases_supplies.js
@@ -1,10 +1,43 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('purchases_supplies').del()
     .then(function () {
-      // Inserts seed entries
       return knex('purchases_supplies').insert([
-        {purchase_id: 1, supplies_id: 1, supplies_qty: 1, supplies_measurement: 'cup', completed: false}
+        {purchase_id: 1, supplies_id: 1, supplies_qty: 1.49, supplies_measurement: 'pnt', completed: false},
+        {purchase_id: 1, supplies_id: 2, supplies_qty: 1.27, supplies_measurement: 'qt', completed: false},
+        {purchase_id: 1, supplies_id: 3, supplies_qty: 1.25, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 1, supplies_id: 4, supplies_qty: 2, supplies_measurement: 'tsp', completed: false},
+        {purchase_id: 1, supplies_id: 7, supplies_qty: 1.33, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 1, supplies_id: 8, supplies_qty: 2.67, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 1, supplies_id: 9, supplies_qty: 1.5, supplies_measurement: 'pnt', completed: false},
+        {purchase_id: 1, supplies_id: 10, supplies_qty: 1.13, supplies_measurement: 'cup', completed: false},
+        {purchase_id: 1, supplies_id: 11, supplies_qty: 5.17, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 1, supplies_id: 12, supplies_qty: 12, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 1, supplies_id: 13, supplies_qty: 2, supplies_measurement: 'tsp', completed: false},
+        {purchase_id: 1, supplies_id: 14, supplies_qty: 2.5, supplies_measurement: 'tsp', completed: false},
+
+        {purchase_id: 2, supplies_id: 1, supplies_qty: 1, supplies_measurement: 'pnt', completed: false},
+        {purchase_id: 2, supplies_id: 2, supplies_qty: 1.33, supplies_measurement: 'pnt', completed: false},
+        {purchase_id: 2, supplies_id: 3, supplies_qty: 1.5, supplies_measurement: 'tsp', completed: false},
+        {purchase_id: 2, supplies_id: 4, supplies_qty: 1, supplies_measurement: 'tsp', completed: false},
+        {purchase_id: 2, supplies_id: 7, supplies_qty: 2, supplies_measurement: 'tsp', completed: false},
+        {purchase_id: 2, supplies_id: 8, supplies_qty: 1.33, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 2, supplies_id: 10, supplies_qty: 6, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 2, supplies_id: 11, supplies_qty:8.33, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 2, supplies_id: 12, supplies_qty: 1.5, supplies_measurement: 'cup', completed: false},
+        {purchase_id: 2, supplies_id: 13, supplies_qty: 1.33, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 2, supplies_id: 14, supplies_qty: .5, supplies_measurement: 'tsp', completed: false},
+
+        {purchase_id: 3, supplies_id: 1, supplies_qty: 1.5, supplies_measurement: 'pnt', completed: false},
+        {purchase_id: 3, supplies_id: 2, supplies_qty: 2, supplies_measurement: 'pnt', completed: false},
+        {purchase_id: 3, supplies_id: 3, supplies_qty: 2.5, supplies_measurement: 'tsp', completed: false},
+        {purchase_id: 3, supplies_id: 4, supplies_qty: 1.5, supplies_measurement: 'tsp', completed: false},
+        {purchase_id: 3, supplies_id: 7, supplies_qty: 1, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 3, supplies_id: 8, supplies_qty: 2, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 3, supplies_id: 10, supplies_qty: 9, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 3, supplies_id: 11, supplies_qty: 12.5, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 3, supplies_id: 12, supplies_qty: 1.13, supplies_measurement: 'pnt', completed: false},
+        {purchase_id: 3, supplies_id: 13, supplies_qty: 2, supplies_measurement: 'Tbs', completed: false},
+        {purchase_id: 3, supplies_id: 14, supplies_qty: .75, supplies_measurement: 'tsp', completed: false}
       ])
     })
 };
