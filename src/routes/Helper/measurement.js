c9ed6cd20f90c67bbc29811da9b9cf6b49370fc9
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-07-16 14:22:33 -0700

    final changes

--- src/routes/Helper/measurement.js
+++ src/routes/Helper/measurement.js
@@ -7,7 +7,6 @@ router.get('/volume', measurementController.volume)
 router.get('/length', measurementController.length)
 router.get('/mass', measurementController.mass)
 router.get('/wrightStream/:shopId', measurementController.wrightStream)
-router.post('/suppliesList', measurementController.predictor)
 router.post('/orderPredictor', measurementController.orderPredictor)
 router.post('/compareOrder/:shopId', measurementController.compareOrderPredictor)
 
